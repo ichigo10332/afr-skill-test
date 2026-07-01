93114183934527e9f581b300f06a1939860c1a44
Mauricio Arnez <arnezmauricio2@gmail.com>
2026-07-01 09:45:38 -0400

    Ejercicio 5: "Usuario: Leanne Graham"

--- web-desarrollo/ejercicio5.js
+++ web-desarrollo/ejercicio5.js
@@ -1,15 +1,16 @@
 // API pública: https://jsonplaceholder.typicode.com/users/1
 // Responde con un objeto que incluye, entre otros campos: { id: 1, name: "Leanne Graham", ... }
-
+const api_publica = "https://jsonplaceholder.typicode.com/users/1";
 async function obtenerUsuario() {
   // TODO: Usa fetch para obtener los datos del usuario con id 1
-  const response = await /* tu código aquí */;
+  const response = await fetch(api_publica);
 
   // TODO: Convierte la respuesta a JSON
-  const data = await /* tu código aquí */;
+  const data = await response.json();
 
   // TODO: Imprime en consola: "Usuario: " seguido del nombre del usuario
   /* tu código aquí */
+  console.log(`Usuario: ${data.name}`);
 }
 
 obtenerUsuario();
